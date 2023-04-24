ef5e3a1da41a9377255a2ef878a6e6ac58efdca0
Erica-Ku <yeskuminji@naver.com>
2023-04-24 12:30:48 +0900

    0424

---
+++ springboot/Toy1/src/main/java/toy1/websocket/SocketHandler.java
@@ -0,0 +1,33 @@
+package toy1.websocket;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession s : sessions) {
+            s.sendMessage(message);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
